117843801db3f8a6372190223a7ed5e726098205
intermine <staylor@cs.uoregon.edu>
2013-04-30 11:07:58 -0700

    commiting changes pre pull

--- zfinmine/dbmodel/build/gen/src/org/intermine/model/bio/Chromosome.java
+++ zfinmine/dbmodel/build/gen/src/org/intermine/model/bio/Chromosome.java
@@ -2,8 +2,4 @@ package org.intermine.model.bio;
 
 public interface Chromosome extends org.intermine.model.bio.SequenceFeature
 {
-    public java.util.Set<org.intermine.model.bio.Gene> getGenes();
-    public void setGenes(final java.util.Set<org.intermine.model.bio.Gene> genes);
-    public void addGenes(final org.intermine.model.bio.Gene arg);
-
 }
