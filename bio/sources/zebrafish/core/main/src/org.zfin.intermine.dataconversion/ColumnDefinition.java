7ee706b05cee8d2b9874c573f99035f187b4b980
Christian Pich <cmpich@zfin.org>
2013-07-26 14:26:20 -0700

    add prefix logic for feature, refactor to use spec sheet for simple converter

---
+++ bio/sources/zebrafish/core/main/src/org.zfin.intermine.dataconversion/ColumnDefinition.java
@@ -0,0 +1,11 @@
+package org.zfin.intermine.dataconversion;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cmpich
+ * Date: 7/25/13
+ * Time: 12:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ColumnDefinition {
+}
