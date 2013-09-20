89a177e1703930ed80a241dbc6cc81cb77cccf14
Christian Pich <cmpich@zfin.org>
2013-09-20 10:31:39 -0700

    replace mrkrseq_zdb_id with mrkr_zdb_id

--- bio/sources/zebrafish/markerSequence/main/src/org/intermine/bio/dataconversion/MarkerSequenceConverter.java
+++ bio/sources/zebrafish/markerSequence/main/src/org/intermine/bio/dataconversion/MarkerSequenceConverter.java
@@ -38,13 +38,10 @@ import org.intermine.xml.full.Item;
 import org.xml.sax.SAXException;
 
 
-
 /**
- * 
  * @author
  */
-public class MarkerSequenceConverter extends BioFileConverter
-{
+public class MarkerSequenceConverter extends BioFileConverter {
     //
     private static final String DATASET_TITLE = "Morpholino and SNP Sequences";
     private static final String DATA_SOURCE_NAME = "ZFIN";
@@ -52,11 +49,12 @@ public class MarkerSequenceConverter extends BioFileConverter
     private static final Logger LOG = Logger.getLogger(MarkerSequenceConverter.class);
     private Map<String, Item> mrkrs = new HashMap();
     private Map<String, Item> seqs = new HashMap();
-  
+
     /**
      * Constructor
+     *
      * @param writer the ItemWriter used to handle the resultant items
-     * @param model the Model
+     * @param model  the Model
      */
     public MarkerSequenceConverter(ItemWriter writer, Model model) {
         super(writer, model, DATA_SOURCE_NAME, DATASET_TITLE);
@@ -66,24 +64,24 @@ public class MarkerSequenceConverter extends BioFileConverter
 
         processSeqs(reader);
 
-        try{
+        try {
 
-            for (Item item : seqs.values()){                                                                                                        
-              store(item);                                                                                                                          
-            }                                                                                                                                       
-            for (Item item : mrkrs.values()){
+            for (Item item : seqs.values()) {
                 store(item);
             }
-        }catch (ObjectStoreException e) {
+            for (Item item : mrkrs.values()) {
+                store(item);
+            }
+        } catch (ObjectStoreException e) {
             throw new SAXException(e);
         }
     }
 
 
     public void processSeqs(Reader reader) throws Exception {
-	
+
         Iterator lineIter = FormattedTextParser.parseDelimitedReader(reader, '|');
-	
+
         while (lineIter.hasNext()) {
             String[] line = (String[]) lineIter.next();
             if (line.length < 3) {
@@ -94,44 +92,41 @@ public class MarkerSequenceConverter extends BioFileConverter
             String seq = line[1];
             String startOffset = line[2];
             String stopOffset = line[3];
-	    String variation = line[4];
-	    String type = line[5];
-	    String mrkrSeqId = line[6];
+            String variation = line[4];
+            String type = line[5];
 
-            if (!StringUtils.isEmpty(dataId)){		
+            if (!StringUtils.isEmpty(dataId)) {
                 Item mrkr = getTypedItem(dataId, type);
-		if (!StringUtils.isEmpty(seq)){
-		    Item sequence = getSeq(mrkrSeqId,seq,variation,startOffset,stopOffset);
-		    mrkr.addToCollection("sequences", sequence);
-		    if (!StringUtils.isEmpty(variation)){
-			sequence.setAttribute("variation",variation);
-		    }
-		    if (!StringUtils.isEmpty(startOffset)){
-			sequence.setAttribute("offsetStart",startOffset);
-		    }
-		    if (!StringUtils.isEmpty(stopOffset)){
-			sequence.setAttribute("offsetEnd",stopOffset);
-		    }
-		}
-		
-	    }
-	
-           
+                if (!StringUtils.isEmpty(seq)) {
+                    Item sequence = getSeq(dataId, seq, variation, startOffset, stopOffset);
+                    mrkr.addToCollection("sequences", sequence);
+                    if (!StringUtils.isEmpty(variation)) {
+                        sequence.setAttribute("variation", variation);
+                    }
+                    if (!StringUtils.isEmpty(startOffset)) {
+                        sequence.setAttribute("offsetStart", startOffset);
+                    }
+                    if (!StringUtils.isEmpty(stopOffset)) {
+                        sequence.setAttribute("offsetEnd", stopOffset);
+                    }
+                }
+
+            }
+
+
         }
     }
 
 
-
     private Item getTypedItem(String primaryIdentifier, String type) throws SAXException {
-        Item typedItem ;
+        Item typedItem;
 
-	if (type.equals("MRPHLNO")) {
-            typedItem = getMrkr(primaryIdentifier,"MorpholinoOligo");
+        if (type.equals("MRPHLNO")) {
+            typedItem = getMrkr(primaryIdentifier, "MorpholinoOligo");
         } else if (type.equals("SNP")) {
-            typedItem = getMrkr(primaryIdentifier,"SNP");
-	}
-	else {
-            typedItem = getMrkr(primaryIdentifier,"Gene");
+            typedItem = getMrkr(primaryIdentifier, "SNP");
+        } else {
+            typedItem = getMrkr(primaryIdentifier, "Gene");
         }
 
         return typedItem;
@@ -144,22 +139,22 @@ public class MarkerSequenceConverter extends BioFileConverter
             item.setReference("organism", getOrganism("7955"));
             item.setAttribute("primaryIdentifier", primaryIdentifier);
             mrkrs.put(primaryIdentifier, item);
-	}
+        }
 
         return item;
     }
 
-    private Item getSeq(String primaryIdentifier, String seq, String variation,String offsetStart,String offsetEnd) throws SAXException {
+    private Item getSeq(String primaryIdentifier, String seq, String variation, String offsetStart, String offsetEnd) throws SAXException {
         Item item = seqs.get(primaryIdentifier);
         if (item == null) {
             item = createItem("Sequence");
             item.setReference("organism", getOrganism("7955"));
             item.setAttribute("identifier", primaryIdentifier);
-	    item.setAttribute("residues",seq);
+            item.setAttribute("residues", seq);
 
-	    seqs.put(primaryIdentifier, item);
-	    
-	}
+            seqs.put(primaryIdentifier, item);
+
+        }
         return item;
     }
 
